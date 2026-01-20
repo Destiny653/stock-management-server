c382866b47466f955f3bc843caf2e1e98fa3692c
Destiny653 <fokundem653@gmail.com>
2026-01-20 01:21:17 +0100

    feat: Conditionally configure CORS origins using `settings.BACKEND_CORS_ORIGINS` and adjust `allow_credentials`.

--- main.py
+++ main.py
@@ -16,13 +16,22 @@ app = FastAPI(
 app.add_middleware(LoggingMiddleware)
 
 # Set all CORS enabled origins
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+if settings.BACKEND_CORS_ORIGINS:
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=[str(origin) for origin in settings.BACKEND_CORS_ORIGINS],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+else:
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["*"],
+        allow_credentials=False,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
 
 # Static files (uploads)
 os.makedirs("uploads/products", exist_ok=True)
